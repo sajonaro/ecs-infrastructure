a4e60c873dec87b00f7b4c6157a8d863dd17c8d7
sajonaro <alex.kunich@gmail.com>
2022-11-04 00:07:49 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "2.70.0"
+    }
+  }
+
+  backend "s3" {
+    bucket =  var.bucket_name
+    key    =  var.bucket_key
+    region =  var.aws_region
+  }
+
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
