2f505596cdd37bafbd288e59c16d64c614040b5e
sajonaro <alex.kunich@gmail.com>
2022-11-08 11:20:08 +0200

    s3 state fixed

--- provider.tf
+++ provider.tf
@@ -13,10 +13,30 @@ provider "aws" {
 
 }
 
+resource "aws_s3_bucket" "tf_state"{
+  bucket = "tfstate-2022"
+  lifecycle {
+    prevent_destroy =true
+  }
+
+  versioning {
+    enabled= true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorythm ="AES256"
+      }
+    }
+  }
+}
+
 terraform {
   backend "s3" {
     bucket = "tfstate-2022"
-    key    = "tfstate"
+    key    = "global/s3/terraform.tfstate"
     region = "eu-north-1"
+    encrypt = true
   }
 }
